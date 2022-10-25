771083d0ba8998e7fe0a20c37448bd35ef2b2f46
I Karos <aotamez1@gmail.com>
2022-10-25 20:52:36 +0700

    fix

--- src/variables.js
+++ src/variables.js
@@ -1,2 +1,3 @@
-export const BACKEND_URL = 'https://xemphim-cloneserver.herokuapp.com';
+// export const BACKEND_URL = 'https://xemphim-cloneserver.herokuapp.com';
+export const BACKEND_URL = 'https://movie-server-levanthi.vercel.app';
 // export const BACKEND_URL = 'http://localhost:8080';
